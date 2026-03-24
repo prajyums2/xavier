156ee045b03fc99dbd188e034c4a5e1e05371077
Prajyu Ms <prajyu2003@gmail.com>
2026-03-24 07:59:45 +0530

    Add favicon from assets

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,9 +3,16 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title:
-    "സേവ്യർ ചിറ്റിലപ്പിള്ളി — LDF സ്ഥാനാർത്ഥി  | വടക്കാഞ്ചേരി നിയോജക മണ്ഡലം",
+    "സേവ്യർ ചിറ്റിലപ്പിള്ളി — LDF സ്ഥാനാർത്ഥി | വടക്കാഞ്ചേരി നിയോജക മണ്ഡലം",
   description:
-    "സേവ്യർ ചിറ്റിലപ്പിള്ളി — LDF സ്ഥാനാർത്ഥി , വടക്കാഞ്ചേരി നിയോജക മണ്ഡലം, LDF",
+    "സേവ്യർ ചിറ്റിലപ്പിള്ളി — LDF സ്ഥാനാർത്ഥി, വടക്കാഞ്ചേരി നിയോജക മണ്ഡലം",
+  icons: {
+    icon: [
+      { url: "/favicon.ico", sizes: "any" },
+      { url: "/favicon-96x96.png", type: "image/png", sizes: "96x96" },
+    ],
+    shortcut: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({
